2b0d5227a954b182fbb92755219f9d82edd25759
chengjie <chengj@123>
2020-08-04 13:00:05 +0800

    fix

---
+++ src/main/java/pers/marscheng/thread/lock/Mutex.java
@@ -0,0 +1,79 @@
+package pers.marscheng.thread.lock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @program: base-study
+ * @description: 独占锁
+ * @author: Marscheng
+ * @create: 2020-08-03 19:36
+ **/
+
+public class Mutex implements Lock {
+    private static class Sync extends AbstractQueuedSynchronizer {
+        // 当状态为0的时候获取锁
+        @Override
+        protected boolean tryAcquire(int arg) {
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        //释放锁，将状态设置为0
+        @Override
+        protected boolean tryRelease(int arg) {
+            if (getState() == 0) throw new IllegalMonitorStateException();
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+
+        Condition newCondition() {
+            return new ConditionObject();
+        }
+    }
+
+    private final Sync sync = new Sync();
+
+
+    @Override
+    public void lock() {
+        sync.tryAcquire(1);
+
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, unit.toNanos(time));
+    }
+
+    @Override
+    public void unlock() {
+        sync.tryRelease(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+}
